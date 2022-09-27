66e689767c5fc3c0383bae54d47189a04fbf4f5a
alfiansyah hidayat <105407023+alfiansyahhidayat@users.noreply.github.com>
2022-09-27 20:12:32 +0700

    Add files via upload

---
+++ Tugas1OOp.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+
+public class Tugas1OOP {
+    public static void main(String[]args){
+        System.out.println("belajar JAVA");
+        System.out.println("sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+        
+         //2
+    String input = JOptionPane.showInputDialog("Matkul apa yang anda pelajari");
+    JOptionPane.showMessageDialog(null, "Belajar" + input + "sangat mudah.");
+        
+               
+    }
+    
+}
